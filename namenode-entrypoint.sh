5524fbdb7a210b34994ae5a29be1adac34a093fb
linjacques <linjacques463@gmail.com>
2025-06-20 17:04:10 +0200

    ca marche thomas ! spark-submit   --master yarn   --deploy-mode client   --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=python3   --conf spark.executorEnv.PYSPARK_PYTHON=python3   --conf spark.hadoop.yarn.resourcemanager.hostname=resourcemanager   preprocessor/main.py

--- namenode-entrypoint.sh
+++ namenode-entrypoint.sh
@@ -10,12 +10,4 @@ fi
 echo "Starting NameNode..."
 /entrypoint.sh hdfs namenode &
 
-echo "⏳ Attente du démarrage de HDFS..."
-sleep 10
-
-echo "📁 Création de /tmp/logs dans HDFS"
-hdfs dfs -mkdir -p /tmp/logs || echo "❌ Échec mkdir"
-hdfs dfs -chown -R yarn:hadoop /tmp/logs || echo "❌ Échec chown"
-hdfs dfs -chmod -R 1777 /tmp/logs || echo "❌ Échec chmod"
-
 wait
