044aaa3eb95a50b9ceb43ed60f027e4d6343424d
linjacques <linjacques463@gmail.com>
2025-06-29 21:18:35 +0200

    fin du projet

--- app/api/main.py
+++ app/api/main.py
@@ -6,7 +6,7 @@ from sqlalchemy import Table, MetaData
 Base.metadata.create_all(bind=engine)
 
 metadata = MetaData()
-metadata.reflect(bind=engine, schema="public")  # ← adapte ici
+metadata.reflect(bind=engine, schema="public")
 table = Table("gold_final_accidents", metadata, autoload_with=engine, schema="public")
 app = FastAPI()
 
@@ -21,3 +21,4 @@ def get_db():
 def table_contents(page: int = 0, limit: int = 1, db: Session = Depends(get_db)):
     results = db.query(table).offset(page).limit(limit).all()
     return [dict(row._mapping) for row in results]
+
